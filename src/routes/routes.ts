938aee0e10bdbf79baa4040c65e3706a0d744045
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-25 20:16:01 -0500

    Routes aninadas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 import { Route } from "../index/index";
-
+import { Layout } from "../01-lazyload/layout/Layout";
+import { Nolazy } from "../01-lazyload/pages";
 const Lazy1 = lazy(
   () =>
     import(/*webpackChunkName: "LazyPage1" */ "../01-lazyload/pages/LazyPage1")
@@ -13,24 +14,19 @@ const Lazy3 = lazy(
   () =>
     import(/*webpackChunkName: "LazyPage3" */ "../01-lazyload/pages/LazyPage3")
 );
+const LazyLayout = lazy(() => import("../01-lazyload/layout/Layout"));
 
 export const routes: Route[] = [
   {
-    to: "/lazy1",
-    path: "lazy1",
-    Component: Lazy1,
-    name: "Lazy-1",
+    to: "/nolazy",
+    path: "nolazy",
+    Component: Nolazy,
+    name: "nolazy",
   },
   {
-    to: "/lazy2",
-    path: "lazy2",
-    Component: Lazy2,
-    name: "Lazy-2",
-  },
-  {
-    to: "/lazy3",
-    path: "lazy3",
-    Component: Lazy3,
-    name: "Lazy-3",
+    to: "/lazyLayout/",
+    path: "/lazyLayout/*",
+    Component: LazyLayout,
+    name: "lazyLayout-2",
   },
 ];
